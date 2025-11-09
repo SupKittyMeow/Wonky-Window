f0e98ca1398d7f8dafef826a864649b4aa41b59c
SupKittyMeow <supkittymeow@gmail.com>
2025-11-08 21:36:02 -0800

    - no rounded window :( - no blur :( - you can now type while dragging the window - respecs system theme - multi monitor support

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtGui import QCursor
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QCursor, QScreen
 from PySide6.QtCore import QTimer, QEvent, Qt
 import qframelesswindow
 import time
@@ -25,26 +25,30 @@ class Window(qframelesswindow.AcrylicWindow):
         super().__init__()
         self.setWindowTitle("Wonky Window")
         self.setFixedSize(0, 0)
-        self.setTitleBar(QWidget()) # oh my god thank you so much chatgpt this took like 2 hours to figure out
-        self.toggleStayOnTop()
-        self.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.setResizeEnabled(False)
+        
+        self.titleBar.hide()
+        
+        self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowDoesNotAcceptFocus | Qt.WindowType.WindowStaysOnTopHint)
 
+        self.setStyleSheet("CustomAcrylicWindow {border-radius: 10px;}") # Library handles the actual OS-level rounding/blur. Without this (idk why) it won't adhere to system theme on Windows
+        
         # updating vars setup
         self.last_time: float = time.perf_counter()
-        self.velX: float = 0
+        self.velX: float = 0 
         self.velY: float = 0
         self.posX: float = 0
         self.posY: float = 0
 
-        self.offsetX = 0
-        self.offsetY = 0
+        self.screen_offsetX = 0
+        self.screen_offsetY = 0
         self.previousFramesX = [0, 0, 0, 0, 0, 0]
         self.previousFramesY = [0, 0, 0, 0, 0, 0]
         self.previousTimestamps = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.mouseDown: bool = False
         
-        self.screen_width: int = self.screen().size().width()
-        self.screen_height: int = self.screen().size().height()
+        self.screen_width: int = self.screen().availableSize().width()
+        self.screen_height: int = self.screen().availableSize().height()
 
         self.hasBeenUnderMousePreviousFrame: bool = False
 
@@ -56,6 +60,12 @@ class Window(qframelesswindow.AcrylicWindow):
         self.move(center_x, center_y)
         self.posX = center_x
         self.posY = center_y
+
+        window = self.windowHandle()
+        if window is not None:
+            window.screenChanged.connect(self.screen_changed)
+
+        self.screen_changed(self.screen())
 
         # start the spawning anim!
         self.spawn_animation()
@@ -113,7 +123,7 @@ class Window(qframelesswindow.AcrylicWindow):
         self.velX = sum(distancesX) / len(distancesX) / THROW_FACTOR
         self.velY = sum(distancesY) / len(distancesY) / THROW_FACTOR
         self.mouseDown = False
-
+    
     def spawn_animation(self, i=1):
         cursorPos = QCursor.pos()
         center_x = cursorPos.x() - 50 # subtracting 50, as this is half of the width of the window (center it instead of top left)
@@ -134,32 +144,47 @@ class Window(qframelesswindow.AcrylicWindow):
             self.last_time = current_time
             QTimer.singleShot(16, lambda: self.spawn_animation(i+int(250 * delta_time)))
 
-    def check_window_collision(self): # TODO: i just copy pasted from the other script, so it no way works
+    def screen_changed(self, screen: QScreen):
+        self.geo = screen.availableGeometry()
+        # self.screen_width: int = geo.width()
+        # self.screen_height: int = geo.height()
+        # self.screen_offsetX: int = geo.x()
+        # self.screen_offsetY: int = geo.y()
+
+    def check_window_collision(self):
         if not self.mouseDown:
-            width = self.screen_width - self.width()
-            height = self.screen_height - self.height()
-
-            if self.posX < 0:
-                distance = (self.posX - 0) / POP_FACTOR
+            self.screen_changed(self.screen())
+            furthestLeft = self.geo.left()
+            furthestUp = self.geo.top()
+            width = self.geo.right() - self.width()
+            height = self.geo.bottom() - self.height()
+            posX = self.posX
+            posY = self.posY
+            
+            if posX < furthestLeft:
+                distance = (posX - furthestLeft) / POP_FACTOR
                 self.velX = (-self.velX * BOUNCE_FACTOR) + distance
-                self.posX = 0
-            elif self.posX > width:
-                distance = (self.posX - width) / POP_FACTOR
+                posX = furthestLeft
+            elif posX > width:
+                distance = (posX - width) / POP_FACTOR
                 self.velX = (-self.velX * BOUNCE_FACTOR) + distance
-                self.posX = width
+                posX = width
 
-            if self.posY > height:
-                distance = (self.posY - height) / POP_FACTOR
+            if posY > height:
+                distance = (posY - height) / POP_FACTOR
                 self.velY = (-self.velY * BOUNCE_FACTOR) + distance
-                self.posY = height
+                posY = height
                 if abs(self.velY) < 0.1:
                     self.velY = 0
-            elif self.posY < 38:
-                distance = (self.posY - 38) / POP_FACTOR
+            elif posY < furthestUp:
+                distance = (posY - furthestUp) / POP_FACTOR
                 self.velY = (-self.velY * BOUNCE_FACTOR) + distance
-                self.posY = 38
+                posY = furthestUp
                 if abs(self.velY) < 0.1:
                     self.velY = 0
+
+            self.posX = posX
+            self.posY = posY
 
     def update_physics(self):
         if self.mouseDown:
@@ -199,8 +224,8 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     refresh_rate = app.primaryScreen().refreshRate()
 
-    wins = []
-    for _ in range(1):
+    wins: list[Window] = []
+    for _ in range(1): # change this number to have different number of windows
         wins.append(Window())
     for win in wins:
         win.show()
